57d92568608fefd384192e0accd775702c0bf4e1
gedadev <gjbg1234@gmail.com>
2023-04-13 22:47:48 -0600

    Add function that encrypts the message
    Call the function from the listener and send the current input value as argument
    Convert the string into lower case, and then into an array and save it into a new constatn
    Loop into the array and replace each letter with the correspondent key
    Return the array as a string

--- main.js
+++ main.js
@@ -6,3 +6,29 @@ const result = document.querySelector('#result');
 btnEncrypt.addEventListener('click', () => {
     encrypt(input.value);
 });
+
+function encrypt(msg) {
+    const arrMsg = msg.toLowerCase().split('');
+    arrMsg.forEach((letter, i) => {
+        switch (letter) {
+            case 'a':
+                arrMsg[i] = 'ai';
+                break;
+            case 'e':
+                arrMsg[i] = 'enter';
+                break;
+            case 'i':
+                arrMsg[i] = 'imes';
+                break;
+            case 'o':
+                arrMsg[i] = 'ober';
+                break;
+            case 'u':
+                arrMsg[i] = 'ufat';
+                break;
+            default:
+                break;
+        }
+    });
+    return arrMsg.join('');
+}
