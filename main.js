6ebfc023c460e7770a98eae865cc566e96feef8a
gedadev <gjbg1234@gmail.com>
2023-04-15 13:44:05 -0600

    Toggle the copy button view
    Set a listener to display the element when the mouse passes over
    Set a listener to hide the element when the mouse goes out

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const input = document.querySelector('#input');
 const btnEncrypt = document.querySelector('#encrypt');
 const btnDecrypt = document.querySelector('#decrypt');
 const result = document.querySelector('#result');
+const btnCopy = document.querySelector('#copy');
 const p = document.createElement('p');
 
 btnEncrypt.addEventListener('click', () => {
@@ -12,6 +13,15 @@ btnEncrypt.addEventListener('click', () => {
 btnDecrypt.addEventListener('click', () => {
     p.textContent = decrypt(input.value);
     result.appendChild(p);
+});
+
+result.addEventListener('mouseover', (e) => {
+    if(result.firstChild.textContent !== '')
+        btnCopy.style.display = 'block';
+});
+
+result.addEventListener('mouseout', (e) => {
+    btnCopy.style.display = 'none';
 });
 
 function encrypt(msg) {
@@ -41,10 +51,15 @@ function encrypt(msg) {
 }
 
 function decrypt(msg) {
-    if (/ai/.test(msg)) msg = msg.split('ai').join('a');
-    if (/enter/.test(msg)) msg = msg.split('enter').join('e');
-    if (/imes/.test(msg)) msg = msg.split('imes').join('i');
-    if (/ober/.test(msg)) msg = msg.split('ober').join('o');
-    if (/ufat/.test(msg)) msg = msg.split('ufat').join('u');
+    if (/ai/.test(msg)) 
+        msg = msg.split('ai').join('a');
+    if (/enter/.test(msg)) 
+        msg = msg.split('enter').join('e');
+    if (/imes/.test(msg)) 
+        msg = msg.split('imes').join('i');
+    if (/ober/.test(msg)) 
+        msg = msg.split('ober').join('o');
+    if (/ufat/.test(msg)) 
+        msg = msg.split('ufat').join('u');
     return msg;
 }
